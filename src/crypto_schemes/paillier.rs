02d90f1e7a25bcfed478f8dfedab931edef59d08
Milan Sedivy <milan.sedivy@hotmail.com>
2024-05-21 22:35:10 +0300

    Destructure Paillier PoC into individual components

--- src/crypto_schemes/paillier.rs
+++ src/crypto_schemes/paillier.rs
@@ -1,10 +1,8 @@
-use std::thread::Thread;
 use num_bigint::{BigInt, BigUint, RandBigInt};
 use num_prime::RandPrime;
 use rand::prelude::ThreadRng;
 use rand::thread_rng;
 use crate::crypto_schemes::bigint::{UsefulOperations, UsefulConstants, ModSub};
-use crate::crypto_schemes::el_gamal::EncryptedMessage;
 #[derive(Debug)]
 pub struct Components {
     pub p: BigUint,
@@ -25,11 +23,24 @@ pub struct KeyPair {
     pub(crate) shares: Vec<BigUint>,
 }
 #[derive(Debug)]
-pub struct Paillier {
+pub struct PaillierGenerator {
     pub components: Components,
     pub key_pair: KeyPair,
     rng: ThreadRng,
     number_of_shares: u8,
+    pub delta: u128
+}
+#[derive(Debug)]
+pub struct PaillierCipher {
+    rng: ThreadRng,
+    pub public_key: PublicKey,
+    secret_share: BigUint,
+    delta: u128
+}
+
+pub struct Combiner {
+    public_key: PublicKey,
+    decrypted_message_shares: Vec<BigUint>,
     delta: u128
 }
 impl KeyPair {
@@ -38,14 +49,50 @@ impl KeyPair {
     }
     pub fn get_shares(&self) -> &Vec<BigUint> {&self.shares}
 }
-impl Paillier {
-    pub fn init(number_of_shares: u8) -> Self {
+
+pub trait Generator {
+    type Output;
+    type Generator;
+    fn init(number_of_shares: u8) -> Self::Generator;
+    //For testing purposes only
+    fn from_data(components: Components, key_pair: KeyPair, number_of_shares: u8, delta: u128) -> Self::Generator;
+    fn create_shares(&mut self);
+    fn f(&self, x: Self::Output, coefficients: &Vec<Self::Output>) -> Self::Output;
+}
+pub trait Group {
+    type Output;
+    fn get_element_of_group(rng: &mut ThreadRng, modulo: &Self::Output) -> Self::Output;
+}
+impl Group for PaillierGenerator {
+    type Output = BigUint;
+    fn get_element_of_group(rng: &mut ThreadRng, modulo: &BigUint) -> BigUint {
+        let mut x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
+        while x.gcd(&modulo) != BigUint::one() {
+            x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
+        }
+        x
+    }
+}
+impl Group for PaillierCipher {
+    type Output = BigUint;
+    fn get_element_of_group(rng: &mut ThreadRng, modulo: &BigUint) -> BigUint {
+        let mut x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
+        while x.gcd(&modulo) != BigUint::one() {
+            x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
+        }
+        x
+    }
+}
+impl Generator for PaillierGenerator {
+    type Output = BigUint;
+    type Generator = Self;
+    fn init(number_of_shares: u8) -> Self {
         let mut rng = thread_rng();
         let one = BigUint::one();
         let two = BigUint::two();
 
-        let p: BigUint = BigUint::from(11u8);//rng.gen_safe_prime(256);
-        let mut q: BigUint = BigUint::from(7u8);//rng.gen_safe_prime(256);
+        let p: BigUint = rng.gen_safe_prime(256);
+        let mut q: BigUint = rng.gen_safe_prime(256);
         while p.eq(&q)
         {
             q = rng.gen_safe_prime(256);
@@ -56,14 +103,12 @@ impl Paillier {
         let N= &p * &q;
         let N_squared = N.pow(2);
         let m= &p_sub * &q_sub;
-        let a = Paillier::get_element_of_group(&mut rng,&N);
-        let b = Paillier::get_element_of_group(&mut rng,&N);
-        let beta = Paillier::get_element_of_group(&mut rng,&N);
+        let a = PaillierGenerator::get_element_of_group(&mut rng,&N);
+        let b = PaillierGenerator::get_element_of_group(&mut rng,&N);
+        let beta = PaillierGenerator::get_element_of_group(&mut rng,&N);
 
-        println!("a: {} b: {} beta: {}", a,b,beta);
         let mut g: BigUint = ((&N+&one).modpow(&a, &N_squared)*(&b.modpow(&N,&N_squared))) % &N_squared;
 
-        //let private_key = (&p-&one).lcm(&(&q-&one));
         let sigma =&a*&m*&beta % &N;
         let public_key = PublicKey {N: N.clone(), g, sigma};
 
@@ -71,40 +116,14 @@ impl Paillier {
 
         let private_key = (&beta*&m) % &(&N*&m);
         let delta = Self::factorial(number_of_shares.clone() as u128);
-        Paillier {components, key_pair: KeyPair {public_key, private_key, shares: Vec::<BigUint>::new()}, rng, number_of_shares, delta}
+        PaillierGenerator {components, key_pair: KeyPair {public_key, private_key, shares: Vec::<BigUint>::new()}, rng, number_of_shares, delta}
     }
-    pub fn from_data(components: Components, key_pair: KeyPair, number_of_shares: u8, delta: u128) -> Self {
+    fn from_data(components: Components, key_pair: KeyPair, number_of_shares: u8, delta: u128) -> Self {
         let mut rng = thread_rng();
 
-        Paillier { components, key_pair, rng, number_of_shares, delta}
+        PaillierGenerator { components, key_pair, rng, number_of_shares, delta}
     }
-    pub fn encrypt(&mut self, message: BigUint) -> BigUint {
-        let m = &self.components.p_sub*&self.components.q_sub;
-
-        let modulo = &self.key_pair.public_key.N;
-        let mut x = Paillier::get_element_of_group(&mut self.rng, &modulo);
-        println!("{}", x);
-        let modulo = modulo.pow(2);
-        (&self.key_pair.public_key.g.modpow(&message,&modulo) * x.modpow(&self.key_pair.public_key.N, &modulo)) % modulo
-    }
-    // pub fn decrypt(&mut self, message: BigUint) -> BigUint {
-    //     let pk = self.key_pair.borrow_pk();
-    //     let modulo = &pk.N.pow(2);
-    //     let rhs = self.L(&message.modpow(&self.key_pair.private_key, &modulo));
-    //     let lhs = self.L(&pk.g.modpow(&self.key_pair.private_key, &modulo));
-    //     let lhs_inv = lhs.modinv(&pk.N).unwrap();
-    //
-    //     (rhs * lhs_inv) % &pk.N
-    // }
-
-    pub fn get_element_of_group(rng: &mut ThreadRng, modulo: &BigUint) -> BigUint {
-        let mut x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
-        while x.gcd(&modulo) != BigUint::one() {
-            x = rng.gen_biguint_range(&BigUint::one(), &(modulo));
-        }
-        x
-    }
-    pub fn create_shares(&mut self) {
+    fn create_shares(&mut self) {
         let mut shares = Vec::<BigUint>::new();
         let mut coefficients = Vec::<BigUint>::new();
         let m = &self.components.p_sub*&self.components.q_sub;
@@ -121,81 +140,100 @@ impl Paillier {
     }
     fn f(&self, x: BigUint, coefficients: &Vec<BigUint>) -> BigUint {
         let m = &self.components.p_sub*&self.components.q_sub;
-        //coefficients.iter().enumerate().for_each(|(i,e)| println!("{i+1}th coef: {}", e));
-        //coefficients.iter().enumerate().map(|(i, a_i)| a_i*x.pow((i+1) as u32)).enumerate().for_each(|(i,e)| println!("{i+1}th a_i * x^i: {}", e));
         let result = (coefficients.iter().enumerate()
             .map(|(i, a_i)| a_i*x.pow((i+1) as u32))
             .map(|e| e % (&self.key_pair.public_key.N*&m))
             .sum::<BigUint>() + &self.key_pair.private_key) % (&self.key_pair.public_key.N*&m);
-        println!("x:{x} - f(x):{result}");
         result
 
-        // for i in 1..coefficients.len() {
-        //     point += &coefficients[i] * x.pow(i as u32);
-        //     point %= &self.key_pair.public_key.N*&m
-        // }
-        // point
+    }
+}
+pub trait Cipher {
+    type Output;
+    fn encrypt(&mut self, message: Self::Output) -> Self::Output;
+    fn decrypt_share(&mut self, message: Self::Output) -> Self::Output;
+}
+
+impl Cipher for PaillierCipher {
+    type Output = BigUint;
+    fn encrypt(&mut self, message: BigUint) -> BigUint {
+        let modulo = &self.public_key.N;
+        let mut x = PaillierCipher::get_element_of_group(&mut self.rng, &modulo);
+        let modulo = modulo.pow(2);
+        (&self.public_key.g.modpow(&message,&modulo) * x.modpow(&self.public_key.N, &modulo)) % modulo
     }
     #[inline]
-    pub fn decrypt_share(&mut self, message: BigUint, sk_share: BigUint) -> BigUint { //This is OK
-        let decrypted_share = message.modpow(&(BigUint::from(2*self.delta)*sk_share), &self.key_pair.public_key.N.pow(2));
-        decrypted_share
+    fn decrypt_share(&mut self, message: BigUint) -> BigUint {
+        message.modpow(&(BigUint::from(2*self.delta)*&self.secret_share), &self.public_key.N.pow(2))
     }
+}
+/*pub struct PaillierCipher {
+    rng: ThreadRng,
+    pub public_key: PublicKey,
+    secret_share: BigUint,
+    delta: u128
+}
 
-    pub fn combine_shares(&mut self, decrypted_shares: Vec<BigUint>) -> BigUint {
-        let m = &self.components.p_sub*&self.components.q_sub;
-        let inner_element: BigUint = decrypted_shares.iter().enumerate().map(|(k, c_k)| {
-            println!("{}", k);
-            let micro = &self.calculate_micro(k+1);
+pub struct Combiner {
+    public_key: PublicKey,
+    decrypted_message_shares: Vec<BigUint>,
+    delta: u128
+}*/
+impl PaillierCipher {
+    pub fn init_from(public_key: &PublicKey, secret_share: &BigUint, delta: u128) -> Self {
+        let rng = thread_rng();
+        Self {rng, public_key: public_key.clone(), secret_share: secret_share.clone(), delta }
+    }
+}
+impl Combiner {
+    pub fn init_from(public_key: &PublicKey, delta: u128) -> Self {
+        let decrypted_message_shares = Vec::<BigUint>::new();
+        Self {public_key: public_key.clone(), decrypted_message_shares, delta }
+    }
+    pub fn add_decrypted_message_share(&mut self, decrypted_share: BigUint) {
+        self.decrypted_message_shares.push(decrypted_share);
+    }
+    pub fn combine_shares(&mut self) -> BigUint {
+        println!("{:?}", self.decrypted_message_shares);
+        let inner_element: BigUint = self.decrypted_message_shares.clone().iter().enumerate().map(|(k, c_k)| {
+            let micro = &self.calculate_micro(k.clone()+1);
             match micro >= &BigInt::from(0) {
                 true => {
-                    let result = c_k.modpow(&(&BigUint::two() * micro.to_biguint().unwrap()), &(&self.key_pair.public_key.N.pow(2)));
-                    println!("micro.to_biguint().unwrap(): {}", &(&BigUint::two() * micro.to_biguint().unwrap()));
-                    println!("c_k^micro*2: {}", result);
+                    let result = c_k.modpow(&(&BigUint::two() * micro.to_biguint().unwrap()), &(&self.public_key.N.pow(2)));
                     return result;
                 },
                 false => {
-                    //let result = c_k.signed_modpow(&(&BigInt::from(2u8) * micro), &(&self.key_pair.public_key.N*&m), &(&self.key_pair.public_key.N.pow(2)));
                     let pos_micro = (BigInt::from(-1) * micro).to_biguint().unwrap() * &BigUint::two();
-                    let result = c_k.modpow(&pos_micro, &self.key_pair.public_key.N.pow(2)).modinv(&self.key_pair.public_key.N.pow(2)).unwrap();
-                    println!("&(&BigInt::from(2u8) * micro: {}", &(&BigInt::from(2u8) * micro));
-                    println!("c_k^micro*2: {}", result);
+                    let result = c_k.modpow(&pos_micro, &self.public_key.N.pow(2)).modinv(&self.public_key.N.pow(2)).unwrap();
                     return result;
                 }
             }
-            //c_k.modpow(&(&BigUint::two() * &self.calculate_micro(k)), &self.key_pair.public_key.N.pow(2))
-        }
-        ).product::<BigUint>();
-        println!("inner_element: {}", inner_element);
-        // M
-        println!("{}", 4*self.delta.pow(2)*&self.key_pair.public_key.sigma % &self.key_pair.public_key.N);
-        let inv = (4*self.delta.pow(2)*&self.key_pair.public_key.sigma % &self.key_pair.public_key.N).modinv(&self.key_pair.public_key.N).unwrap();
-        println!("AHA!");
-        (self.L(&(&inner_element % &self.key_pair.public_key.N.pow(2))) * inv) % &self.key_pair.public_key.N
-    }
+        }).product::<BigUint>();
 
-    pub fn calculate_micro(&mut self, k: usize) -> BigInt {
+        let inv = (4*self.delta.pow(2)*&self.public_key.sigma % &self.public_key.N).modinv(&self.public_key.N).unwrap();
+        (self.L(&(&inner_element % &self.public_key.N.pow(2))) * inv) % &self.public_key.N
+    }
+    fn calculate_micro(&mut self, k: usize) -> BigInt {
         let mut micro = (self.delta as i128);
 
-        //let m = &self.components.p_sub*&self.components.q_sub;
-        for l in 1..=self.key_pair.shares.len() {
+        for l in 1..=self.decrypted_message_shares.len() {
             if l == k { continue }
             micro /= l as i128 - k as i128;
             micro *= l as i128 ;
         }
-        let result: BigInt = BigInt::from(micro);
-        println!("micro: {}, k: {}", result, k);
-        result
+        BigInt::from(micro)
     }
+
+    #[allow(non_snake_case)]
+    #[inline]
+    fn L(&self, u: &BigUint) -> BigUint {
+        (u - BigUint::one()) / &self.public_key.N
+    }
+}
+impl PaillierGenerator {
     #[inline]
     fn factorial(num: u128) -> u128 {
         (1..=num).product()
     }
-    #[allow(non_snake_case)]
-    #[inline]
-    fn L(&self, u: &BigUint) -> BigUint {
-        //println!("L: {}", (u - BigUint::one()) / &self.key_pair.public_key.N);
-        println!("{}", u);
-        (u - BigUint::one()) / &self.key_pair.public_key.N
-    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ops::DerefMut;
 use num_bigint::{BigInt, BigUint, ToBigInt};
 use sha256::digest;
 use crate::crypto_schemes::el_gamal::{Encryption, Signature};
-use crate::crypto_schemes::paillier::{KeyPair, Paillier, PublicKey};
+use crate::crypto_schemes::paillier::{Cipher, Combiner, Generator, KeyPair, PaillierCipher, PaillierGenerator, PublicKey};
 
 mod crypto_schemes;
 mod utils;
@@ -25,28 +25,36 @@ fn main() {
     // let signature = el_gamal.sign(String::from("Hello"));
     // println!("{}", el_gamal.verify(String::from("Hello"), signature));
 
-    let mut paillier : Paillier = Paillier::init(3);
+    let mut paillier = PaillierGenerator::init(4);
     println!("{:?}", paillier);
-    // let components = crypto_schemes::paillier::Components {p: BigUint::from(11u8), q: BigUint::from(7u8), p_sub: BigUint::from(5u8), q_sub: BigUint::from(3u8)};
-    // let key_pair = KeyPair { public_key: PublicKey { N: BigUint::from(77u32), g: BigUint::from(3974u32), sigma: BigUint::from(74u32) }, private_key: BigUint::from(345u32), shares: vec![BigUint::from(574u32), BigUint::from(989u32), BigUint::from(198u32), BigUint::from(274u32)] };
-    // let mut paillier: Paillier = Paillier::from_data(components, key_pair, 4, 24);
-    let t = BigUint::from(5u8);
-    let encrypted = paillier.encrypt(t);
-
     paillier.create_shares();
+    let mut paillier_combiner = Combiner::init_from(&paillier.key_pair.public_key, paillier.delta.clone());
+    //paillier.key_pair.shares.iter().for_each(|e|)
+
+    let mut cipher_engines: Vec<PaillierCipher> = vec![];
+    paillier.key_pair.shares.iter().for_each(|e| cipher_engines.push(PaillierCipher::init_from(&paillier.key_pair.public_key, e, paillier.delta.clone())));
+    println!("{:?}", cipher_engines);
+    println!("{}", cipher_engines.len());
+    let t = BigUint::from(5u8);
+    let encrypted = cipher_engines[1].encrypt(t);
+    println!("{}", encrypted);
+    cipher_engines.iter_mut().for_each(|e| paillier_combiner.add_decrypted_message_share(e.decrypt_share(encrypted.clone())));
+
+    println!("{}", paillier_combiner.combine_shares());
+
     // paillier.create_shares(4u8);
-    let mut shares: Vec<BigUint> = paillier.key_pair.get_shares().clone().iter_mut().map(|e| paillier.decrypt_share(encrypted.clone(),e.clone())).collect();
-    println!("c (encrypted): {}", encrypted);
-    println!("Individual shares: {:?}", paillier.key_pair.get_shares());
-    println!("Decrypted share of message: {:?}", shares);
-    // println!("{}", BigUint::from(4160u32).modpow(&BigUint::from(36u8),&BigUint::from(5929u32)));
-    // println!("{}", BigUint::from(449u32).modinv(&BigUint::from(5929u32)).unwrap());
-    let shares_again = paillier.key_pair.get_shares().clone()
-        .iter_mut()
-        .enumerate()
-        .map(|(i, e)| &paillier.calculate_micro(i+1)*&e.clone().to_bigint().unwrap()).sum::<BigInt>();
-    println!("shares_again: {}", shares_again);
-    let result = paillier.combine_shares(shares);
-    //let result = paillier.decrypt(encrypted);
-    println!("{}", result);
+    // //let mut shares: Vec<BigUint> = paillier.key_pair.get_shares().clone().iter_mut().map(|e| paillier.decrypt_share(encrypted.clone(),e.clone())).collect();
+    // println!("c (encrypted): {}", encrypted);
+    // println!("Individual shares: {:?}", paillier.key_pair.get_shares());
+    // println!("Decrypted share of message: {:?}", shares);
+    // // println!("{}", BigUint::from(4160u32).modpow(&BigUint::from(36u8),&BigUint::from(5929u32)));
+    // // println!("{}", BigUint::from(449u32).modinv(&BigUint::from(5929u32)).unwrap());
+    // let shares_again = paillier.key_pair.get_shares().clone()
+    //     .iter_mut()
+    //     .enumerate()
+    //     .map(|(i, e)| &paillier.calculate_micro(i+1)*&e.clone().to_bigint().unwrap()).sum::<BigInt>();
+    // println!("shares_again: {}", shares_again);
+    // let result = paillier.combine_shares(shares);
+    // //let result = paillier.decrypt(encrypted);
+    // println!("{}", result);
 }
